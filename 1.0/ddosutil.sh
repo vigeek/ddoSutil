9bc1ed61cdcf3b07b7269c3ccdfc64ade7155ed0
Russ Thompson <vigornj@gmail.com>
2011-04-10 21:55:38 -0400

    update 1.0

--- 1.0/ddosutil.sh
+++ 1.0/ddosutil.sh
@@ -38,6 +38,8 @@ eecho () {
 # Firewall Builder					 #
 ###################################
 
+PROGRAM="FWBuilder"
+
 if [ $FW_BUILDER -eq "1" ] ; then
 
 # Flush the current rules.
@@ -60,6 +62,8 @@ fi
 # DShield Blocker						 #
 ###################################
 
+PROGRAM="DShield"
+
 if [ $DSHIELD_BLOCK="1" ] ; then
 	wget -q -O 'dshield.txt' http://feeds.dshield.org/block.txt
 		if [ ! -f dshield.txt ] ; then
@@ -74,7 +78,55 @@ fi
 # GeoIP Blocker						 #
 ###################################
 
+PROGRAM="geoIP"
+
+ddosutil_geoip () {
 if [ $GEOIP_BLOCK = "1" ] ; then
 
-
+	if [ $USE_MIRROR -eq 1 ] ; then
+		GET_SUCCESS=$(curl -s -w %{size_download} -o 'GeoIPCountryCSV.zip' -z GeoIPCountryCSV.zip 
+http://geolite.maxmind.com/download/geoip/database/GeoIPCountryCSV.zip)
+	else
+		GET_SUCCESS=$(curl -s -w %{size_download} -o 'GeoIPCountryCSV.zip' -z GeoIPCountryCSV.zip http://vigeek.net/files/GeoIPCountryCSV.zip)
+	fi
+	    if [ $GET_SUCCESS -eq 0 ] ; then
+			 log "Local version of GeoIP list current, no download required."
+	    else
+			 log "Local version outdated, downloaded latest GeoIP list total transfer: $GET_SUCCESS"
+	    fi
+	fi
+	
+	# Trim spaces if any from our csv list
+	SAVE_FILE="ddosutil-geoip.lst"
+	# Check if our chain exists, if not, create it.
+	$IP_TABLES -L ddoSutil &> /dev/null
+		if [ $? -eq 0 ] ; then
+			$IP_TABLES -N ddoSutil
+	   fi
+	   
+	# Build our list.
+	# Add for loop (trim space and csv from geo_list)
+	zcat GeoIPCountryCSV.zip | grep -i "$COUNTRY_NAME" | cut -d "," -f1,2 | sed 's/,/-/g' | tr -d '"' > $SAVE_FILE
+	    if [ -f $SAVE_FILE ] ; then
+			 if [ $(cat $SAVE_FILE | wc -l) -eq 0 ] ; then
+				log "Error: no geoIP list built, is this correct: $COUNTRY_NAME"
+	       else
+				log "Completed building geoIP list for $COUNTRY_NAME total records $(cat $COUNTRY_NAME | wc -l)"
+	            echo -en "\e[1;31mdSutil:\033[0m working, please wait."
+	            for i in `cat $SAVE_FILE`; do
+	              $IP_TABLES -A ddoSutil -s 0/0 -d 0/0 -m iprange --src-range $i -j DROP
+	              let COUNTER+=1
+						 if [ $(( $COUNTER % 2 )) -eq 0 ]; then
+							echo -n "."
+	                fi
+					done
+					ask_them "Begin blocking connections from $COUNTRY_NAME?"
+	                if ($USER_ACTION) ; then
+	                    $IP_TABLES -I INPUT -j $SAVE_FILE
+	                    $IP_TABLES -I FORWARD -j $SAVE_FILE
+	                fi
+	       fi
+		fi
 fi
+}
+
