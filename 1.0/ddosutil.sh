ad250c7faabac4fd9fe11aece6acbc0d690149fa
Russ Thompson <vigornj@gmail.com>
2011-04-21 00:29:58 -0400

    1.0 many additions

--- 1.0/ddosutil.sh
+++ 1.0/ddosutil.sh
@@ -11,10 +11,11 @@ SUCCESS=0
 CHAIN="ddoSutil"
 
 # This holds a string for logging to identify which utility is logging what.
-PROGRAM=""
+PROGRAM="main"
 
 # Which iptables.
 IP_TABLES=`which iptables`
+IP_TABLES_SAVE=`which iptables-save`
 
 # Source the config.
 if [ -f "ddosutil.conf" ]
@@ -24,18 +25,47 @@ else
 	exit $FAILED
 fi
 
-# Global Functions
 
-# Simple logging function
+################################################
+# General Functions & Init
+################################################
+
 log () {
     if [ -n "$VERBOSE" ] ; then eecho "$1" ; fi
-echo "$(date +%m-%d-%Y\ %H:%M:%S) | $PROGRAM | $1" >> $LOG_FILE
+echo "$(date +%m-%d-%Y\ %H:%M:%S) | [$PROGRAM] | $1" >> $LOG_FILE
 }
+
+# Create initial log entry
+log "ddoSutil has kicked off"
 
 # Add some color.
 eecho () {
     echo -e "\e[1;31mddoSutil:\033[0m $1"
 }
+
+# See if CSF/LFD is running.
+CSFD=`pidof lfd`
+	if [ -n "$CSFD" ] ; then
+		log "CSF [lfd] daemon detected"
+		eecho "The CSF [lfd] daemon is currently running..."
+		eecho "If using the ddoSutil firewall this may overwrite our rules"
+		eecho "The CSF [lfd] daemon can be temporarily disabled:  csf --disable"
+	fi
+
+# See if APF is installed.
+APFD=`pidof apfd`
+	if [ -n "$APFD" ] ; then
+		log "APF [apfd] daemon detected"
+		eecho "The APF [apfd] daemon is currently running..."
+		eecho "If using the ddoSutil firewall this may overwrite our rules"
+		eecho "APF can be temporarily disabled by stopping the service"
+	fi
+
+# Whatever the current rules are attempt to back them up.
+if [ ! -f iptables.back ] ; then
+	log "saving current iptable rules to iptables.back"
+	$IP_TABLES_SAVE > iptables.back
+fi	
 
 ################################################
 # Firewall Builder 
@@ -44,6 +74,7 @@ eecho () {
 PROGRAM="FWBuilder"
 
 if [ $FW_BUILDER -eq "1" ] ; then
+	log "firewall builder is enabled, building rules."
 
 	# Flush the current rules.
 	$IP_TABLES -F &> /dev/null
@@ -62,13 +93,13 @@ if [ $FW_BUILDER -eq "1" ] ; then
 			log "Unable to create iptable rules"
 			return $FAILED
 		fi
-
 	# Accept All Out
 	$IP_TABLES -P OUTPUT ACCEPT
 	# Drop ALL In
 	$IP_TABLES -P INPUT DROP
 	# Accept lo
 	$IP_TABLES -A INPUT -i lo -j ACCEPT
+	log "flushed the firewall and created baseline policy"
 	# Drop INVALID packets
 	if [ $DROP_INVALID -eq "1" ] ; then $IP_TABLES -A INPUT -m state --state INVALID -j DROP ; fi
 	# Accept all in Established Related State
@@ -81,10 +112,12 @@ if [ $FW_BUILDER -eq "1" ] ; then
 	if [ $DROP_XMAS -eq "1" ] ; then $IP_TABLES -A INPUT -p tcp --tcp-flags ALL ALL -j DROP ; fi
 	# Drop NULL packets
 	if [ $DROP_NULL -eq "1" ] ; then $IP_TABLES -A INPIT -p tcp --tcp-flags ALL NONE -j DROP ; fi
+	log "enabled baseline drop rules for undesirable packets"
 
 	# Begin building a simple firewall
 	for IFACE in ${IFACE_LIST//,/ } ; do
-
+		log "Opening requested ports on [$IFACE]"
+		
 		for tcpin ${TCP_IN//,/ } ; do
 			$IP_TABLES -A INPUT -p tcp -i $IFACE --dport $tcpin -m state --state NEW -j ACCEPT
 		done
@@ -105,8 +138,8 @@ fi
 ################################################
 
 PROGRAM="DShield"
-
 if [ $DSHIELD_BLOCK="1" ] ; then
+ log "enabling dshield block list"
 	wget -q -O 'dshield.lst' http://feeds.dshield.org/block.txt
 		if [ ! -f dshield.lst ] ; then
 			eecho "Error downloading dshield list"
@@ -127,6 +160,7 @@ fi
 PROGRAM="ZeusBlock"
 
 if [ $ZEUS_BLOCK="1" ] ; then
+ log "enabling zeus block list"
 	wget -q -O 'zeustrack.lst' --no-check-certificate https://zeustracker.abuse.ch/blocklist.php?download=ipblocklist
 		if [ ! -f zeustrack.lst ] ; then
 			eecho "Error downloading zeus tracker list"
@@ -148,6 +182,7 @@ fi
 PROGRAM="viGeek"
 
 if [ $VIGEEK_BLOCK="1" ] ; then
+ log "enabling viGeek block list"
 	wget -q -O 'vigeek.lst' http://vigeek.net/projects/ddoSutil/vigeek.txt
 		if [ ! -f vigeek.lst ] ; then
 			eecho "Error downloading dshield list"
@@ -168,6 +203,7 @@ fi
 PROGRAM="IANA"
 
 if [ $IANA_BLOCK -eq "1" ] ; then
+ log "blocking IANA reserved ip ranges"
 	$IP_TABLES -N $PROGRAM
 	#
 	$IP_TABLES -A $PROGRAM -s 0.0.0.0/7 -j DROP
@@ -192,21 +228,41 @@ if [ $IANA_BLOCK -eq "1" ] ; then
 	$IP_TABLES -A $PROGRAM -s 182.0.0.0/8 -j DROP
 	$IP_TABLES -A $PROGRAM -s 185.0.0.0/8 -j DROP
 	$IP_TABLES -A $PROGRAM -s 223.0.0.0/8 -j DROP
-	$IP_TABLES -A $PROGRAM -s 224.0.0.0/4 -j DROP
 	$IP_TABLES -A $PROGRAM -s 240.0.0.0/4 -j DROP
 	#
 	$IP_TABLES -A $PROGRAM -j RETURN
 fi
 
 ################################################
-# GeoIP Blocker #
+# Cymru Bogon Blocker 
+################################################
+
+PROGRAM="bogon"
+
+if [ $BOGON_BLOCK="1" ] ; then
+ log "enabling team-cymru.org bogon block list"
+	wget -q -O 'bogon.lst' http://www.team-cymru.org/Services/Bogons/bogon-bn-nonagg.txt
+		if [ ! -f bogon.lst ] ; then
+			eecho "Error downloading bogon list"
+		return $FAILURE
+		fi
+	# Begin creaitng list
+	$IP_TABLES -N $PROGRAM
+	for dlist in `cat bogon.lst | grep -v '#'` ; do
+        $IP_TABLES -A $PROGRAM -s $dlist -j DROP
+	done
+		$IP_TABLES -A $PROGRAM -j RETURN
+fi
+
+################################################
+# GeoIP Blocker 
 ################################################
 
 PROGRAM="geoIP"
 
 ddosutil_geoip () {
 if [ $GEOIP_BLOCK = "1" ] ; then
-
+ log "enabling geoIP blocking"
 	if [ $USE_MIRROR -eq 1 ] ; then
 		GET_SUCCESS=$(curl -s -w %{size_download} -o 'GeoIPCountryCSV.zip' -z GeoIPCountryCSV.zip \
 		http://geolite.maxmind.com/download/geoip/database/GeoIPCountryCSV.zip)
@@ -293,6 +349,8 @@ if [ $SYN_LIMIT -eq "1" ] ; then
 	
 fi
 
+if [ $SYN_COOKIES -eq "1" ] ; then echo 1 > /proc/sys/net/ipv4/tcp_syncookies ; fi
+
 ################################################
 # Timeout Reductions + others.				 
 ################################################
@@ -302,4 +360,40 @@ if [ $TIMEOUT_REDUCE -eq "1" ; then
 	echo 1800 > /proc/sys/net/ipv4/tcp_keepalive_time
 	echo 0 > /proc/sys/net/ipv4/tcp_sack
 	echo 0 > /proc/sys/net/ipv4/tcp_window_scaling 
+fi
+
+# Set back log queue, first verify our new value is greater than old.
+CUR_VALUE=`cat /proc/sys/net/ipv4/tcp_max_syn_backlog`
+if [ $CUR_VALUE -gt $BACKLOG_QUEUE ] ; then
+	eecho "Desired backlog value $BACKLOG_QUEUE is less than $CUR_VALUE"
+fi
+
+################################################
+# Basic spoof protection.				 
+################################################
+
+if [ $SPOOF_PROTECT -eq "1" ] ; then
+
+	# Ancient host.conf modifications.
+	if [ -z "$(grep 'order hosts,bind' /etc/host.conf)" ] ; then
+		echo -e "order hosts,bind" >> /etc/host.conf
+	fi
+	
+	if [ -z "$(grep 'multi on' /etc/host.conf)" ] ; then
+		echo -e "multi on" >> /etc/host.conf
+	fi
+	
+	if [ -z "$(grep 'nospoof on' /etc/host.conf)" ] ; then
+		echo -e "nospoof on" >> /etc/host.conf
+	fi
+	
+	if [ -z "$(grep 'spoofalert on' /etc/host.conf)" ] ; then
+		echo -e "spoofalert on" >> /etc/host.conf
+	fi
+	
+	# Enable reverse path filter on all interfaces.
+	for rpf in /proc/sys/net/ipv4/conf/*/rp_filter; do
+		echo 1 > $rpf
+	done
+
 fi
